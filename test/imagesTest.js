3d4466f79af81d27b25d79ec73b9f45f3c5c6e7a
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2016-06-05 14:32:19 +0200

    add support for encoding into plain base64 without data uri

--- test/imagesTest.js
+++ test/imagesTest.js
@@ -7,6 +7,8 @@ describe('images', function () {
   var reporter
 
   beforeEach(function (done) {
+    // looks like a current bug in jsreport-express, it should start on random port by default
+    process.env.PORT = 0
     reporter = new Reporter({
       rootDirectory: path.join(__dirname, '../')
     })
@@ -53,7 +55,7 @@ describe('images', function () {
       })
   })
 
-  it('should replace image tag with base64 content', function (done) {
+  it('should replace image tag with full base64 content', function (done) {
     reporter.images.upload('test withSpace', 'image/jpeg', new Buffer([1, 2, 3]))
       .then(function () {
         var request = {}
@@ -67,5 +69,45 @@ describe('images', function () {
           done()
         }).catch(done)
       }).catch(done)
+  })
+
+  it('should replace image tag with plain base64 content', function (done) {
+    reporter.images.upload('foo', 'image/jpeg', new Buffer([1, 2, 3]))
+      .then(function () {
+        var request = {}
+
+        var response = {
+          content: new Buffer('a{#image foo @encoding=base64}')
+        }
+
+        reporter.images.handleAfterTemplatingEnginesExecuted(request, response).then(function () {
+          assert.equal(response.content.toString(), 'a' + new Buffer([1, 2, 3]).toString('base64'))
+          done()
+        }).catch(done)
+      }).catch(done)
+  })
+
+  it('should throw when inserting image with name containing @', function (done) {
+    reporter.documentStore.collection('images').insert({
+      name: 'foo@',
+      content: 'foo'
+    }).then(function () {
+      done(new Error('Should have failed'))
+    }).catch(function () {
+      done()
+    })
+  })
+
+  it('should throw when updating image with name containing @', function (done) {
+    reporter.documentStore.collection('images').insert({
+      name: 'foo',
+      content: 'foo'
+    }).then(function () {
+      return reporter.documentStore.collection('images').update({ name: 'foo' }, { $set: { name: 'foo@' } }).then(function () {
+        done(new Error('Should have failed'))
+      })
+    }).catch(function () {
+      done()
+    })
   })
 })
