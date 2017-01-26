7f7ea8203d5aceec014649549d8f108c7795614a
BJR Matos <bjrmatos@gmail.com>
2017-01-26 15:46:08 -0500

    add default entity tree position

--- studio/main_dev.js
+++ studio/main_dev.js
@@ -2,7 +2,14 @@ import ImageEditor from './ImageEditor.js'
 import ImageUploadButton from './ImageUploadButton.js'
 import Studio from 'jsreport-studio'
 
-Studio.addEntitySet({ name: 'images', faIcon: 'fa-camera', visibleName: 'image', onNew: ImageUploadButton.OpenUpload,
-  helpUrl: 'http://jsreport.net/learn/images' })
+Studio.addEntitySet({
+  name: 'images',
+  faIcon: 'fa-camera',
+  visibleName: 'image',
+  onNew: ImageUploadButton.OpenUpload,
+  helpUrl: 'http://jsreport.net/learn/images',
+  entityTreePosition: 600
+})
+
 Studio.addEditorComponent('images', ImageEditor)
 Studio.addToolbarComponent(ImageUploadButton)
